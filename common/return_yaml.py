801998c1b3a9579d81387b8e4f659342ad6ac826
yuyang <yuyang110221@126.com>
2021-03-09 16:12:17 +0800

    更新测试框架，支持多项目复用

---
+++ common/return_yaml.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+  @Time : 2021/2/20 16:31 
+  @Auth : 于洋
+  @File : yaml.py
+  @IDE  : PyCharm
+  @Motto: ABC(Always Be Coding)
+-------------------------------------------------
+"""
+import yaml
+from common.path import *
+
+def read_yaml(path):
+    '''
+    path: 读取文件路径
+    title：文件数据对象标题
+    '''
+    with open(path, encoding='utf-8') as y:
+        data = yaml.load(y, Loader=yaml.FullLoader)
+    return data
+
+
+
+
